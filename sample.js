5e52af031beb0911baf76bf6aa0afbd195db5f5f
kppage <77250352+kppage@users.noreply.github.com>
2021-04-30 21:13:38 -0400

    Final Edits

--- sample.js
+++ sample.js
@@ -30,48 +30,5 @@ $(document).ready(function(){
     });
 });
 
-var edtechtool = {};
-  edtechtool['gsuite'] = 0;
-  edtechtool['flipgrid'] = 0;
-  edtechtool['seesaw'] = 0;
-  edtechtool['nearpod'] = 0;
-$(document).ready(function(){
-  
-  drawChart();
-  
-  $("#submit").click(function(){
-      if($("#edtechtool").val()=="gsuite"){
-        edtechtool['gsuite']++;
-      }
-      if($("#edtechtool").val()=="flipgrid"){
-          edtechtool['flipgrid']++;
-       }
-       if($("#edtechtool").val()=="seesaw"){
-        edtechtool['seesaw']++;
-     }
-     if($("#edtechtool").val()=="nearpod"){
-        edtechtool['nearpod']++;
-     }
-    drawChart();
-  });
-  
-  function drawChart(){
-    var ctx = $('#myChart');
-    var myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: ['GSuite', 'Flipgrid', 'Seesaw', 'Nearpod'],
-            datasets: [{
-                label: '# of Votes',
-                data: [edtechtool['gsuite'], edtechtool['flipgrid'], edtechtool['seesaw'], edtechtool['nearpod']],
-              backgroundColor:[
-                'red',
-                'orange',
-                'yellow',
-                'green',
-              ]
-            }]
-            }
-        });
-  }
-  });
+
+
